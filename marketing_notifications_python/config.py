838f00e6a28a417b372a3230a81ea8871c565b0c
nebtrx <neb.trx@gmail.com>
2015-12-10 15:51:38 -0500

    Update project configuration

--- marketing_notifications_python/config.py
+++ marketing_notifications_python/config.py
@@ -2,15 +2,14 @@ class DefaultConfig(object):
     SECRET_KEY = '%^!@@*!&$8xdfdirunb52438#(&^874@#^&*($@*(@&^@)(&*)Y_)((+'
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
+    TWILIO_ACCOUNT_SID = 'your_twilio_account_sid'
+    TWILIO_AUTH_TOKEN = 'your_twilio_auth_token'
+    TWILIO_NUMBER = 'your_twilio_phone_number'
 
 
 class DevelopmentConfig(DefaultConfig):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
-
-    TWILIO_ACCOUNT_SID = 'your_twilio_account_sid'
-    TWILIO_AUTH_TOKEN = 'your_twilio_auth_token'
-    TWILIO_NUMBER = 'your_twilio_phone_number'
 
 
 class TestConfig(DefaultConfig):
